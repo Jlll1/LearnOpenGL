78d3a023f5fdbb3f8f363f73f4bcd924052407b0
Jlll1 <arghantentua@tutanota.com>
2022-05-22 16:40:38 +0200

    HelloWindow - 1

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gettingstarted
+
+go 1.18
+
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw v0.0.0-20220516021902-eb3e265c7661 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220516021902-eb3e265c7661 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func main() {
+	runtime.LockOSThread()
+
+	defer glfw.Terminate()
+
+	glfw.Init()
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+
+	window, err := glfw.CreateWindow(800, 600, "LearnOpenGL", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+	gl.Init()
+
+	framebufferSizeCallback := func(win *glfw.Window, w, h int) {
+		gl.Viewport(0, 0, int32(w), int32(h))
+	}
+	window.SetFramebufferSizeCallback(framebufferSizeCallback)
+
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
